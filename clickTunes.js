01328a425ce64aaad3a5e87de16f412178bb7f9f
arief-dolphinsoft <arief@dolphinsoft.co.il>
2024-10-11 18:11:28 +0300

    re-styled the whole extension like chrome dark mode + added add and remove buttons with no functionality

--- clickTunes.js
+++ clickTunes.js
@@ -3,12 +3,31 @@ const filter = document.querySelector('#filter');
 const volume = document.querySelector('#volume');
 const buttonsContainer = document.querySelector('#buttonsContainer');
 const tooltip = document.querySelector('#tooltip');
+const removeButton = document.querySelector('#removeButton');
+// const canvas = document.querySelector('#visualizer');
+// const canvasCtx = canvas.getContext('2d');
 
 // variables
 let isSoundPlaying = false;
 let currentAudio = null;
+let isDeleteModeOn = false;
 
 // functions
+function handleDeleteButton() {
+
+}
+
+function toggleButtonDelete() {
+  isDeleteModeOn = !isDeleteModeOn;
+
+  removeButton.textContent = isDeleteModeOn ? "Cancel" : "Remove Button"
+
+  const deleteButtons = document.querySelectorAll('.delete-btn');
+  deleteButtons.forEach(button => {
+    button.style.display = isDeleteModeOn ? "block" : "none";
+  });
+}
+
 function createButton(buttonObj) {
   const button = document.createElement("button");
   button.name = buttonObj.name.replace(" ", "");
@@ -22,7 +41,12 @@ function createButton(buttonObj) {
     button.disabled = true; // Disable the button when sound starts playing
 
     const sound = new Audio(buttonObj.soundPath);
-    sound.volume = volume.value; // Set the volume to the current slider value
+    const currentVolume = volume.value;
+    if(currentVolume > 0) {
+      sound.volume = volume.value / 100;
+    } else {
+      sound.volume = 0;
+    }
     sound.play();
     currentAudio = sound;
 
@@ -32,6 +56,15 @@ function createButton(buttonObj) {
       currentAudio = null; // Clear the reference when audio ends
     });
   });
+
+  const deleteBtn = document.createElement("span");
+  deleteBtn.textContent = "X";
+  deleteBtn.classList.add('delete-btn');
+  deleteBtn.dataset.name = buttonObj;
+  deleteBtn.addEventListener('click', handleDeleteButton);
+
+  button.appendChild(deleteBtn);
+
   buttonsContainer.appendChild(button);
 }
 
@@ -49,12 +82,33 @@ function filterButtons() {
   });
 }
 
+function updateVolumeBackground() {
+  const value = volume.value;
+  const min = volume.min || 0;
+  const max = volume.max || 100;
+
+  // Calculate the percentage of the slider's current value
+  const percentage = ((value - min) / (max - min)) * 100;
+
+  // keep the dark blue at 5 percent to prevent color changing to the left of slider ball
+  const adjustedPercentage = percentage <= 5 ? 5 : percentage;
+
+  // Apply the background using a linear gradient to show the filled part and unfilled part
+  volume.style.background = `linear-gradient(to right, #062E6F ${adjustedPercentage}%, #A8C7FA ${adjustedPercentage}%)`;
+}
+
 // event listeners
 filter.addEventListener('input', filterButtons);
 
 volume.addEventListener('input', () => {
+  updateVolumeBackground();
   if (currentAudio) {
-    currentAudio.volume = volume.value;
+    const currentVolume = volume.value;
+    if(currentVolume > 0) {
+      currentAudio.volume = volume.value / 100;
+    } else {
+      currentAudio.volume = 0;
+    }
   }
 });
 
@@ -82,7 +136,11 @@ volume.addEventListener('mouseleave', () => {
   tooltip.style.display = 'none';
 });
 
+removeButton.addEventListener('click', toggleButtonDelete);
+
 // execution
+updateVolumeBackground();
+
 fetch('/buttons.json')
 .then(result => result.json())
 .then(buttons => {
